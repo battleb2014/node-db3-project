f19a11c955d818e1be3617f06ff15d0315e87c19
Brandon May <battleb2014@gmail.com>
2021-11-03 19:43:24 -0400

    middleware done

--- api/server.js
+++ api/server.js
@@ -6,5 +6,10 @@ const server = express();
 
 server.use(express.json());
 server.use('/api/schemes', SchemeRouter);
+server.use((err, req, res) => {
+    res.status(err.status || 500).json({
+        message: err.message
+    })
+})
 
 module.exports = server;
